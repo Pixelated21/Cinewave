5deb6666d6693617dae6259dc86818107d045f80
Proximity <javaughnbailey21@gmail.com>
2023-05-26 17:48:20 -0500

    refactor: optimized for re usability

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,10 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function getLanguage(iso_639_1: string): Language | string {
+
+type LanguageTransTypes = "iso_639_1" | "english_name" | "name";
+
+export function getLanguage(iso_639_1: string, to: LanguageTransTypes): string {
   const language = languages.find((language) => language.iso_639_1 === iso_639_1);
-  return language ? { ...language } : iso_639_1;
+  return language ? language[to] : iso_639_1;
 }
