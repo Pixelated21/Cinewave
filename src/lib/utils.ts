59f81451d12f50cdbd2f8d1008f98bdafc4bdfc5
Proximity <javaughnbailey21@gmail.com>
2023-05-26 16:44:05 -0500

    feat: added utility to parse language object

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,13 @@
+import { languages } from "@/data/languages"
+import { Language } from "@/typescript/interfaces";
 import { ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
- 
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export function getLanguage(iso_639_1: string): Language | string {
+  const language = languages.find((language) => language.iso_639_1 === iso_639_1);
+  return language ? { ...language } : iso_639_1;
 }
