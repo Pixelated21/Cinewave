4914d7a63c653c1c20c3d63ad84e079e017f828e
Proximity <javaughnbailey21@gmail.com>
2023-05-26 16:50:12 -0500

    feat: implemented Navigation Bar component

---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,70 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export interface NavigationBarProps extends React.HTMLAttributes<HTMLDivElement> { }
+
+export const links: NavLink[] = [
+    {
+        title: 'Home',
+        href: '/home',
+    },
+    {
+        title: 'Movies',
+        href: '/movies'
+    },
+    {
+        title: 'Try Me',
+        href: '/try-me'
+    },
+    {
+        title: 'Series',
+        href: '/series'
+    },
+    {
+        title: 'Profile',
+        href: '/profile',
+        is_auth: false
+    }
+]
+
+export default function NavigationBar({ className, ...props }: NavigationBarProps) {
+    return (
+        // TODO: Sync transition perfectly with the hero section
+        <nav className={`${className}`}>
+            <div className="max-w-7xl mx-auto h-[72px] flex items-center w-full">
+                <div className="flex justify-between items-center h-full w-full">
+                    <div className="flex gap-x-16 items-center">
+                        <div>
+                            <Image className="object-contain" src={'/logo.svg'} alt="logo" width={189} height={40} />
+                        </div>
+                        <div className="h-10 w-44 bg-white"></div>
+                    </div>
+                    <div className="flex items-center gap-x-8">
+                        <ul className="flex items-center gap-x-8">
+                            {links.map((link) => (
+                                <NavigationLink key={link.title} title={link.title} href={link.href} />
+                            ))}
+                        </ul>
+                        <div className="h-10 w-28 bg-white rounded-sm grid place-items-center">
+                            <span className="text-sm font-bold text-black">Logout</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export interface NavLink {
+    title: string,
+    href: string,
+    is_auth?: boolean
+}
+
+export const NavigationLink = (link: NavLink) => {
+    return (
+        <li className="text-sm text-white">
+            <Link href={link.href}>{link.title}</Link>
+        </li>
+    )
+}
