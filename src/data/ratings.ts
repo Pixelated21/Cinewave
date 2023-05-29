730c7901519119499cbba172de72a324edd6f24f
Proximity <javaughnbailey21@gmail.com>
2023-05-29 07:41:41 -0700

    feat: added Ratings data

---
+++ src/data/ratings.ts
@@ -0,0 +1,17 @@
+import { Rating } from "@/typescript/interfaces";
+
+export const ratings: Rating[] = [
+    {
+        value: 5,
+        name: "I Have High Taste"
+    },
+    {
+        value: 3,
+        name: "Watchable"
+    },
+    {
+        value: 1,
+        name: "I Don't Really Care"
+    },
+]
+
