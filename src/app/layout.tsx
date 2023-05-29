7adeab7b2fce3eabbb7ce7b90659a7a5a419f87a
Proximity <javaughnbailey21@gmail.com>
2023-05-29 07:36:18 -0700

    feat: implemented Breakpoint component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Breakpoints from '@/components/utils/breakpoints'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Breakpoints />
+      </body>
     </html>
   )
 }
