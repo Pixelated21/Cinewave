1a2c27bc798682561d09b1069164e381ef8e0b0b
Proximity <javaughnbailey21@gmail.com>
2023-05-29 08:57:19 -0700

    style: adjusted responsiveness, added input component

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -39,10 +39,10 @@ export default function NavigationBar({ className, ...props }: NavigationBarProp
                         <Link href="/home">
                             <Image className="object-contain" src={'/logo.svg'} alt="logo" width={189} height={40} />
                         </Link>
-                        <div className="h-10 w-44 bg-white"></div>
+                        <div className="h-10 w-44 bg-white lg:block hidden"></div>
                     </div>
                     <div className="flex items-center gap-x-8">
-                        <ul className="flex items-center gap-x-8">
+                        <ul className="md:flex items-center gap-x-8 hidden">
                             {links.map((link) => (
                                 <NavigationLink key={link.title} title={link.title} href={link.href} />
                             ))}
