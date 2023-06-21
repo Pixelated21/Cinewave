a29926dd83ddceb2614181a63ace30227d02fa1f
Proximity <javaughnbailey21@gmail.com>
2023-06-20 19:04:48 -0500

    feat: layout revamp

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,52 +1,113 @@
 import axios from "axios";
-import { DiscoverMovieAdvancedFilters, Genre, Movie, Trending } from "@/typescript/interfaces";
+import {
+    DiscoverMovieAdvancedFilters,
+    Genre,
+    Movie,
+    Trending,
+} from "@/typescript/interfaces";
 import TrendingCard from "@/components/cards/TrendingCard";
 import HeroSection from "@/components/sections/HeroSection";
 import NavigationBar from "@/components/NavigationBar";
 import { genres } from "@/data/genres";
-import GenreSection from "@/components/sections/home/GenreSection";
-
-const fetchDiscoverMovies = async ({ filter }: { filter: DiscoverMovieAdvancedFilters }) => {
-    const res = await axios.get(`https://api.themoviedb.org/3/discover/movie?language=en-US`, {
-        params: {
-            api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
-            ...filter
-        }
-    })
-    return res.data
-}
+import MovieSection from "@/components/sections/home/MovieSection";
 
 const fetchTrendingMovies = async (genres?: string | number[]) => {
-    const res = await axios.get(`https://api.themoviedb.org/3/trending/movie/day?language=en-US`, {
-        params: {
-            api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
-            with_genres: genres
+    const res = await axios.get(
+        `https://api.themoviedb.org/3/trending/movie/day?language=en-US`,
+        {
+            params: {
+                api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
+                with_genres: genres,
+            },
         }
-    })
-    return res.data
-}
+    );
+    return res.data;
+};
 
-const parseGenres = (genres: Genre[]) => ([...genres].splice(0, 5))
+const fetchLatestMovies = async ({
+    filter,
+}: {
+    filter: DiscoverMovieAdvancedFilters;
+}) => {
+    const res = await axios.get(
+        `https://api.themoviedb.org/3/discover/movie?language=en-US`,
+        {
+            params: {
+                api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
+                ...filter,
+            },
+        }
+    );
+    return res.data;
+};
 
-export default async function HomePage(
-    { searchParams }: { searchParams: DiscoverMovieAdvancedFilters }) {
+const fetchLatestSeries = async ({
+    filter,
+}: {
+    filter: DiscoverMovieAdvancedFilters;
+}) => {
+    const res = await axios.get(
+        `https://api.themoviedb.org/3/discover/tv?language=en-US`,
+        {
+            params: {
+                api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
+                ...filter,
+            },
+        }
+    );
+    return res.data;
+};
 
-    const filteredGenres = parseGenres(genres)
-    const moviesData = fetchDiscoverMovies({ filter: { ...searchParams } })
-    const trendingData = fetchTrendingMovies()
-    const [movies, trending] = await Promise.all([moviesData, trendingData])
+const fetchComingSoonMovies = async () => {
+    const res = await axios.get(
+        `https://api.themoviedb.org/3/movie/upcoming?language=en-US`,
+        {
+            params: {
+                api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
+            },
+        }
+    );
+    return res.data;
+};
 
-    const filteredTrending = [...trending.results].splice(0, 10).filter((movie: Movie) => movie.poster_path !== null && movie.backdrop_path !== null)
+const parseGenres = (genres: Genre[]) => [...genres].splice(0, 5);
+
+export default async function HomePage({
+    searchParams,
+}: {
+    searchParams: DiscoverMovieAdvancedFilters;
+}) {
+    const filteredGenres = parseGenres(genres);
+
+    const trendingData = fetchTrendingMovies();
+    const latestMoviesData = fetchLatestMovies({ filter: { ...searchParams } });
+    const latestSeriesData = fetchLatestSeries({ filter: { ...searchParams } });
+    const comingSoonData = fetchComingSoonMovies();
+
+    const [trendingMovies, latestMovies, latestSeries, comingSoon] =
+        await Promise.all([
+            trendingData,
+            latestMoviesData,
+            latestSeriesData,
+            comingSoonData,
+        ]);
+
+    const filteredTrending = [...trendingMovies.results]
+        .splice(0, 10)
+        .filter(
+            (movie: Movie) =>
+                movie.poster_path !== null && movie.backdrop_path !== null
+        );
 
     return (
         <main className=" h-screen overflow-y-auto">
-            <div className="relative h-[571px] xl:h-[671px] shadow-md">
-                <NavigationBar className="z-10 absolute max-w-7xl left-0 right-0 mx-auto w-full" />
+            <div className="relative h-[571px] shadow-md xl:h-[671px]">
+                <NavigationBar className="absolute left-0 right-0 z-10 mx-auto w-full max-w-7xl" />
                 <HeroSection trending={filteredTrending} />
             </div>
 
             {/* Trending Section */}
-            <section className="bg-[#18181B]">
+            {/* <section className="bg-[#18181B]">
                 <div className="mx-auto max-w-7xl py-[60px] px-4 sm:px-8 xl:px-2">
                     <div className="flex flex-col gap-y-5">
                         <div className="flex justify-between items-center">
@@ -64,16 +125,25 @@ export default async function HomePage(
                         </div>
 
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-2.5 gap-y-2.5">
-                            {trending.results.splice(0, 3).map((item: Trending) => (
+                            {trendingMovies.results.splice(0, 3).map((item: Trending) => (
                                 <TrendingCard key={item.id} movie={item} />
                             ))}
                         </div>
                     </div>
                 </div>
-            </section>
+            </section> */}
 
-            {/* Genres Section */}
-            <GenreSection movies={movies} genres={filteredGenres} />
+            {/* Trending */}
+            <MovieSection movies={trendingMovies} title={"Trending"} />
+
+            {/* Latest Movies */}
+            <MovieSection movies={latestMovies} title={"Latest Movies"} />
+
+            {/* Latest TV Show */}
+            <MovieSection movies={latestSeries} title={"Latest TV Shows"} />
+
+            {/* Coming Soon */}
+            <MovieSection movies={comingSoon} title={"Coming Soon"} />
         </main>
-    )
+    );
 }
