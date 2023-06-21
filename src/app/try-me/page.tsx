fb30d3d459da6eb2f5c4b1902976f7ef93d4f514
Proximity <javaughnbailey21@gmail.com>
2023-06-20 19:05:33 -0500

    refactor: formatted

--- src/app/try-me/page.tsx
+++ src/app/try-me/page.tsx
@@ -9,32 +9,34 @@ import { Movie } from "@/typescript/interfaces";
 import axios from "axios";
 
 const fetchTrendingMovies = async () => {
-    const res = await axios.get(`${process.env.NEXT_PUBLIC_THE_MOVIE_DATABASE_API_URL}/trending/movie/day`, {
-        params: {
-            api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
+    const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_THE_MOVIE_DATABASE_API_URL}/trending/movie/day`,
+        {
+            params: {
+                api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
+            },
         }
-    })
-    return res.data
-}
+    );
+    return res.data;
+};
 
 export default async function TryMe() {
-
-    const movies = await fetchTrendingMovies()
+    const movies = await fetchTrendingMovies();
 
     const tryMeCarrouselData = {
-        movies: movies.results
-    }
+        movies: movies.results,
+    };
 
     const tryMeRandomizerData = {
         genres,
-        ratings
-    }
+        ratings,
+    };
 
     return (
-        <main className="bg-[#18181B] h-screen overflow-y-auto">
+        <main className="h-screen overflow-y-auto bg-[#18181B]">
             <NavigationBar className="primary" />
             <TryMeRandomizer data={tryMeRandomizerData} />
             {/* <TryMeCarrousel data={tryMeCarrouselData} /> */}
         </main>
-    )
+    );
 }
