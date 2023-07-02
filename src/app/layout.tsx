888218e3009fac16d4f7edcb962e121d98466f70
Proximity <javaughnbailey21@gmail.com>
2023-07-02 14:52:39 -0500

    feat: implemented '@vercel/analytics'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import Breakpoints from "@/components/utils/Breakpoints";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import ScrollToTop from "@/components/utils/ScrollToTop";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
                 {children}
                 {/* <Breakpoints /> */}
                 {/* <ScrollToTop /> */}
+                <Analytics/>
             </body>
         </html>
     );
