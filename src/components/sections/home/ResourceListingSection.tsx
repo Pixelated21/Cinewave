6d63d880abb4126995f99c3507decbedec7fc55c
Proximity <javaughnbailey21@gmail.com>
2023-07-02 18:14:28 -0500

    fix: corrected HTML semantics

--- src/components/sections/home/ResourceListingSection.tsx
+++ src/components/sections/home/ResourceListingSection.tsx
@@ -27,14 +27,14 @@ export default function ResourceListingSection({
         <section className="bg-[#18181B]">
             <div className="mx-auto max-w-7xl px-4 py-[30px] sm:px-8 xl:px-2">
                 <div className="flex flex-col gap-y-5">
-                    <ul className="flex flex-row items-center gap-x-10 overflow-hidden">
+                    <div className="flex flex-row items-center gap-x-10 overflow-hidden">
                         <div className="group">
                             <h2 className="cursor-pointer text-2xl font-semibold text-gray-500 duration-300 group-hover:text-gray-100">
                                 {title}
                             </h2>
                             <span className="h-0.5 w-0 bg-blue-700 duration-300 group-hover:w-full"></span>
                         </div>
-                    </ul>
+                    </div>
 
                     <LayoutSection>
                         {filteredResource.map((resource: Movie) => (
