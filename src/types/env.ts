edd423a42f8bda40f26e6ccae6e7387d7e48c4e0
Proximity <javaughnbailey21@gmail.com>
2023-07-02 05:00:40 -0500

    feat: added type hinting for ENV

---
+++ src/types/env.ts
@@ -0,0 +1,8 @@
+declare namespace NodeJS {
+    interface ProcessEnv {
+        readonly THE_MOVIE_DATABASE_API_KEY: string;
+        readonly NEXT_PUBLIC_APP_URL: string;
+        readonly NEXT_PUBLIC_API_URL: string;
+        readonly NEXT_PUBLIC_THE_MOVIE_DATABASE_API_URL: string;
+    }
+}
