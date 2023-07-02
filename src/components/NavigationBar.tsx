d4eba9f7e78d564a521f78b941448279b1ef83d6
Proximity <javaughnbailey21@gmail.com>
2023-07-02 04:36:26 -0500

    feat: abstracted navlinks to config, fixed types import

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,61 +1,51 @@
-import { NavLink } from "@/typescript/interfaces";
+import { NavLink } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
 import MovieSearchInput from "./ui/search";
+import { navigationLinks } from "@/app/config/site";
 
-export interface NavigationBarProps extends React.HTMLAttributes<HTMLDivElement> { }
+export interface NavigationBarProps
+    extends React.HTMLAttributes<HTMLDivElement> {}
 
-export const links: NavLink[] = [
-    {
-        title: 'Movies',
-        href: '/home',
-    },
-    {
-        title: 'Try Me',
-        href: '/try-me'
-    },
-    {
-        title: 'Series',
-        href: '/home',
-    },
-    // {
-    //     title: 'Share Space',
-    //     href: '/home',
-    //     is_auth: true,
-    // },
-    {
-        title: 'Profile',
-        href: '/profile',
-        is_auth: true
-    }
-]
-
-export default function NavigationBar({ className, ...props }: NavigationBarProps) {
+export default function NavigationBar({ className }: NavigationBarProps) {
     return (
         <nav className={`${className}`}>
-            <div className="max-w-7xl mx-auto h-[72px] z-30 flex items-center w-full">
-                <div className="flex justify-between items-center h-full w-full px-4 sm:px-8 xl:px-2">
-                    <div className="flex gap-x-16 items-center">
+            <div className="container z-30 flex h-[72px] w-full items-center">
+                <div className="flex h-full w-full items-center justify-between px-4 sm:px-8 xl:px-2">
+                    <div className="flex items-center gap-x-16">
                         <Link href="/home">
-                            <Image className="object-contain" src={'/logo.svg'} alt="logo" width={189} height={40} />
+                            <Image
+                                className="object-contain"
+                                src={"/logo.svg"}
+                                alt="logo"
+                                width={189}
+                                height={40}
+                            />
                         </Link>
                         <MovieSearchInput />
                     </div>
                     <div className="flex items-center gap-x-8">
-                        <ul className="md:flex items-center gap-x-8 hidden">
-                            {links.map((link) => (
-                                <NavigationLink key={link.title} title={link.title} href={link.href} />
+                        <ul className="hidden items-center gap-x-8 md:flex">
+                            {navigationLinks.map((link) => (
+                                <NavigationLink
+                                    key={link.title}
+                                    title={link.title}
+                                    href={link.href}
+                                />
                             ))}
                         </ul>
-                        <Button asChild className="bg-white hover:bg-gray-100  text-purple-700 font-semibold">
+                        <Button
+                            asChild
+                            className="bg-white font-semibold  text-purple-700 hover:bg-gray-100"
+                        >
                             <Link href="/home">Login</Link>
                         </Button>
                     </div>
                 </div>
             </div>
         </nav>
-    )
+    );
 }
 
 export const NavigationLink = (link: NavLink) => {
@@ -63,5 +53,5 @@ export const NavigationLink = (link: NavLink) => {
         <li className="text-sm text-white">
             <Link href={link.href}>{link.title}</Link>
         </li>
-    )
-}
+    );
+};
