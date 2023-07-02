296fa4ac8c0f67d9d8675492ca3cc36db5f047b2
Proximity <javaughnbailey21@gmail.com>
2023-07-02 04:38:14 -0500

    refactor: removed redundant MovieGridLayout

--- src/components/layouts/MovieGridLayout.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export default function MovieGridLayout({ children }: { children: ReactNode }) {
-    return (
-        <div className="grid gap-x-3 gap-y-5 sm:gap-2 md:gap-3 lg:gap-4 xl:gap-5 2xl:gap-5 grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-4">
-            {children}
-        </div>
-    )
-}
