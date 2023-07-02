0132d0c9b6bef11fae9d5c14508440b72741ec62
Proximity <javaughnbailey21@gmail.com>
2023-07-02 04:19:31 -0500

    feat: implemented ScrollToTop component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,27 @@
-import Breakpoints from '@/components/utils/Breakpoints'
-import './globals.css'
-import { Inter } from 'next/font/google'
+import Breakpoints from "@/components/utils/Breakpoints";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import ScrollToTop from "@/components/utils/ScrollToTop";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'CineWave',
-  description: 'Keep track of your favorite movies and TV shows.',
-}
+    title: "CineWave",
+    description: "Keep track of your favorite movies and TV shows.",
+};
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
-        <Breakpoints />
-      </body>
-    </html>
-  )
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                {children}
+                <Breakpoints />
+                <ScrollToTop />
+            </body>
+        </html>
+    );
 }
