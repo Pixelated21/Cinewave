0b83cddd8b8fd8e5ccd1c0e651196b4dd12d5afc
Proximity <javaughnbailey21@gmail.com>
2023-07-02 18:12:54 -0500

    feat: disabled vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
                 {children}
                 {/* <Breakpoints /> */}
                 {/* <ScrollToTop /> */}
-                <Analytics/>
+                {/* <Analytics/> */}
             </body>
         </html>
     );
