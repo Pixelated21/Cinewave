2c9210fe26e4599a2793b8dd62bacff6acca92a2
Proximity <javaughnbailey21@gmail.com>
2023-07-02 19:03:26 -0500

    feat: added series actions

--- src/app/_actions/series.ts
+++ src/app/_actions/series.ts
@@ -15,6 +15,19 @@ export async function getLatestSeriesAction({ filter }: { filter: DiscoverMovieA
     return res.data;
 };
 
+export async function getTrendingSeriesAction() {
+    const res = await axios.get(
+        `${process.env.NEXT_PUBLIC_THE_MOVIE_DATABASE_API_URL}/trending/tv/day`,
+        {
+            params: {
+                api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
+                language: 'en-US',
+            },
+        }
+    );
+    return res.data;
+};
+
 export async function getDiscoverSeriesAction() {
     const res = await axios.get(
         `${process.env.NEXT_PUBLIC_THE_MOVIE_DATABASE_API_URL}/discover/tv`,
@@ -27,3 +40,48 @@ export async function getDiscoverSeriesAction() {
     );
     return res.data;
 };
+
+export async function getSeriesDetailsAction({ id }: { id: string | number }) {
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_THE_MOVIE_DATABASE_API_URL}/tv/${id}`,
+        {
+            params: {
+                api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
+                language: "en-US",
+            },
+        });
+    return res.data;
+};
+
+export async function getSeriesCreditsAction({ id }: { id: string | number }) {
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_THE_MOVIE_DATABASE_API_URL}/tv/${id}/credits`,
+        {
+            params: {
+                api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
+                language: "en-US",
+            },
+        });
+    return res.data;
+};
+
+export async function getSeriesVideosAction({ id }: { id: string | number }) {
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_THE_MOVIE_DATABASE_API_URL}/tv/${id}/videos`,
+        {
+            params: {
+                api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
+                language: "en-US",
+            },
+        });
+    return res.data;
+};
+
+export async function getSeriesRecommendationsAction({ id }: { id: string | number }) {
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_THE_MOVIE_DATABASE_API_URL}/tv/${id}/recommendations`,
+        {
+            params: {
+                api_key: process.env.THE_MOVIE_DATABASE_API_KEY,
+                language: "en-US",
+            },
+        });
+    return res.data;
+};
+
