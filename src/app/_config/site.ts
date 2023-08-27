3889eb9bfcb08acad48846291cb2bde3e02d007a
Proximity <javaughnbailey21@gmail.com>
2023-08-27 06:28:51 -0500

    fix: enabled invisible links

--- src/app/_config/site.ts
+++ src/app/_config/site.ts
@@ -1,36 +1,36 @@
 import { NavLink } from "@/types";
 
 export const navigationLinks: NavLink[] = [
-    {
-        title: "Share Space",
-        href: "/share-space",
-        is_auth: true,
-        is_visible: false,
-        isDisabled: true,
-    },
-    {
-        title: "Try Me",
-        href: "/try-me",
-        isDisabled: true,
-        is_visible: false,
-    },
-    {
-        title: "Movies",
-        href: "/movie",
-        isDisabled: true,
-        is_visible: false,
-    },
-    {
-        title: "Series",
-        href: "/series",
-        isDisabled: true,
-        is_visible: false,
-    },
-    {
-        title: "Profile",
-        href: "/profile",
-        is_auth: true,
-        isDisabled: true,
-        is_visible: false,
-    },
+	{
+		title: "Share Space",
+		href: "/share-space",
+		is_auth: true,
+		is_visible: true,
+		isDisabled: true,
+	},
+	{
+		title: "Try Me",
+		href: "/try-me",
+		isDisabled: true,
+		is_visible: true,
+	},
+	{
+		title: "Movies",
+		href: "/movie",
+		isDisabled: true,
+		is_visible: true,
+	},
+	{
+		title: "Series",
+		href: "/series",
+		isDisabled: true,
+		is_visible: true,
+	},
+	{
+		title: "Profile",
+		href: "/profile",
+		is_auth: true,
+		isDisabled: false,
+		is_visible: true,
+	},
 ];
