08276d070b92cb1ec5da4d3de06e9ab46d437bd9
Proximity <javaughnbailey21@gmail.com>
2023-09-17 06:02:48 -0500

    chore: make typescript happy

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,5 @@
+import { authOptions } from "@/app/_config/auth";
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
 
-export const authOptions = {
-	providers: [
-		GithubProvider({
-			clientId: process.env.GITHUB_ID ?? "",
-			clientSecret: process.env.GITHUB_SECRET ?? "",
-		}),
-	],
-};
-
-export const handler = NextAuth(authOptions);
+export const handler = NextAuth(authOptions) as never;
 export { handler as GET, handler as POST };
