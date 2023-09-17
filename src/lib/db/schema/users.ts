5e353d2a07b7bbecc8c3ca43158768c1e8be2e04
Proximity <javaughnbailey21@gmail.com>
2023-09-17 13:35:38 -0500

    build: added additional schema for authentication

---
+++ src/lib/db/schema/users.ts
@@ -0,0 +1,9 @@
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+
+export const users = pgTable("user", {
+	id: text("id").notNull().primaryKey(),
+	name: text("name"),
+	email: text("email").notNull(),
+	emailVerified: timestamp("emailVerified", { mode: "date" }),
+	image: text("image"),
+});
