7e6c14ce8f0cef1458674d88a69f2d0a5cd0fd28
Proximity <javaughnbailey21@gmail.com>
2023-09-17 05:03:36 -0500

    chore: removed unwanted utilities

--- src/components/utils/Breaker.tsx
+++
@@ -1,5 +0,0 @@
-export default function Breaker({ height = '20px', width = '20px' }: { height?: string, width?: string }) {
-    return (
-        <div aria-hidden style={{ height, width }}/>
-    )
-}
