1575eeae16047477f20381caeb7b4b4dd0426c79
Proximity <javaughnbailey21@gmail.com>
2023-09-22 13:44:45 -0500

    feat: redirect to home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,6 @@
 import Link from 'next/link'
-import AddToWatchListButton from '@/components/resource-add-to-watchlist-button'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-
-  return (
-    <main className="flex min-h-screen items-center justify-center p-24">
-      <div className='flex flex-col gap-y-4 items-center'>
-        <Link href={'/home'} className='font-semibold text-2xl'>Click Here 🥹</Link>
-      </div>
-    </main>
-  )
+  redirect('/home')
 }
